1eae14d6342187496e94bf50fe7e3ec909d2de1b
Antas Jain <jainantas.99@gmail.com>
2024-11-08 14:37:58 -0500

    project Setup

---
+++ src/main/java/tech/antasjain/quitterAi/QuitterAiApplication.java
@@ -0,0 +1,13 @@
+package tech.antasjain.quitterAi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuitterAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuitterAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tech/antasjain/quitterAi/QuitterAiApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.antasjain.quitterAi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuitterAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
